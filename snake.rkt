8b0faca60af730a7bbb67dfca9491d512a596979
Pierre Thierry <pierre@nothos.net>
2020-06-06 14:34:01 +0200

    Add snake growth

--- snake.rkt
+++ snake.rkt
@@ -2,12 +2,20 @@
 (require 2htdp/image)
 (require 2htdp/universe)
 
+
+
+;;;;;;;;;;;;;;;;
+; Données
+;;;;;;;;;;;;;;;;
+
 (define-struct monde
-  (serpent   ; liste de points
-   boni      ; liste de points
-   dir       ; direction du serpent
+  (serpent           ; liste de points
+   boni              ; liste de points
+   dir               ; direction du serpent
    perdu?
-   stop))
+   stop
+   nouveaux-segments ; nombre de segments restant à ajouter
+   ))
 
 (define-struct point (x y))
 
@@ -16,14 +24,26 @@
        (= (point-y p1) (point-y p2))))
 
 
-(define m0 (make-monde
-            (list (make-point 3 0) (make-point 2 0) (make-point 1 0) (make-point 0 0))
-            (list)
-            "right"
-            #f
-            #f))
 
-(define m0+ (struct-copy monde m0 [boni (list (make-point 2 2))]))
+
+
+
+(define (m0)
+  (let ((serpent0 (list (make-point 3 0) (make-point 2 0) (make-point 1 0) (make-point 0 0))))
+    (make-monde
+     serpent0
+     (list (make-bonus serpent0) (make-bonus serpent0) (make-bonus serpent0))
+     "right"
+     #f
+     #f
+     1)))
+
+
+
+;;;;;;;;;;;;;;;;
+; Logique du jeu
+;;;;;;;;;;;;;;;;
+
 
 (define (déplacer-tête précédent dir)
   (cond
@@ -44,11 +64,63 @@
   (cons (déplacer-tête (first précédent) dir)
         (déplacer-corps précédent)))
 
+(define (grandir-serpent précédent dir)
+  (cons (déplacer-tête (first précédent) dir)
+        précédent))
+
+
+(define (monde/serpent-déplacé m)
+  (let* ((déplacer? (= (monde-nouveaux-segments m) 0))
+         (serpent2 (if déplacer?
+                       (déplacer-serpent (monde-serpent m) (monde-dir m))
+                       (grandir-serpent (monde-serpent m) (monde-dir m)))))
+    (struct-copy monde m
+                 (serpent serpent2)
+                 (perdu? (collision? serpent2))
+                 (nouveaux-segments (if déplacer?
+                                        (monde-nouveaux-segments m)
+                                        (sub1 (monde-nouveaux-segments m)))))))
+
+
+(define (make-bonus serpent [boni (list)])
+  (let ((bonus (make-point (random taille-jeu) (random taille-jeu))))
+    (if (or (member bonus serpent point=?)
+            (member bonus boni point=?))
+        (make-bonus serpent boni)
+        bonus)))
+; (append (list 1 2) (list 3 4)) ==> (list 1 2 3 4)
+
+(define (monde/bonus-mangé m)
+  (let* ((tête (first (monde-serpent m)))
+         (boni (monde-boni m))
+         (nouveau-bonus (make-bonus (monde-serpent m) boni)))
+    (if (member tête boni point=?)
+        (struct-copy monde m (boni (cons nouveau-bonus (remove tête boni point=?))))
+        m)))
+
+
+(define (collision? roger)
+  (member (first roger) (rest roger) point=?))
+
+(define (suivant m)
+  (if (monde-perdu? m)
+      m
+      (monde/bonus-mangé (monde/serpent-déplacé m))))
+
+
+
+;;;;;;;;;;;;;;;;
+; Dessin
+;;;;;;;;;;;;;;;;
 
 (define taille-carreau 25)
 (define taille-jeu 20)
 
-(define scene (square (* taille-carreau taille-jeu) "solid" "pink"))
+(define (make-canvas color)
+  (square (* taille-carreau taille-jeu) "solid" color))
+
+(define scene (make-canvas "pink"))
+(define canvas (make-canvas "transparent"))
 
 
 (define carreau (square taille-carreau "solid" "black"))
@@ -58,40 +130,46 @@
 
 (define dessiner-segment dessiner-carreau)
 
-(define (dessiner-serpent fond serpent)
-  (foldl dessiner-segment fond serpent))
+(define (dessiner-serpent serpent)
+  (foldl dessiner-segment canvas serpent))
 
 
 (define dessiner-bonus dessiner-carreau)
 
-(define (dessiner-boni fond boni)
-  (foldl dessiner-bonus fond boni))
+(define (dessiner-boni boni)
+  (foldl dessiner-bonus canvas boni))
 
+(define half-width (/ (image-width canvas) 2))
+(define half-height (/ (image-height canvas) 2))
 
-(define (dessiner-fin perdu? dessin)
+(define (dessiner-fin perdu?)
   (if perdu?
-      (place-image (text "FIN" 48 "red") (/ (image-width dessin) 2) (/ (image-height dessin) 2) dessin)
-      dessin))
+      (place-image (text "FIN" 48 "red") half-width half-height canvas)
+      canvas))
 
 (define (dessiner monde)
-  (dessiner-fin (monde-perdu? monde)
-                (dessiner-boni (dessiner-serpent scene (monde-serpent monde))
-                               (monde-boni monde))))
+  (overlay
+   (dessiner-fin (monde-perdu? monde))
+   (dessiner-boni (monde-boni monde))
+   (dessiner-serpent (monde-serpent monde))
+   scene))
 
+
+
+;;;;;;;;;;;;;;;;
+; Debug
+;;;;;;;;;;;;;;;;
 
 (define (render-serpent serpent)
   (foldl (lambda (pt str) (string-append str "{" (number->string (point-x pt)) "," (number->string (point-y pt)) "} ")) "" serpent))
 
-(define (collision? roger)
-  (member (first roger) (rest roger) point=?))
 
-(define (suivant m)
-  (if (monde-perdu? m)
-      m
-      (let ((serpent2 (déplacer-serpent (monde-serpent m) (monde-dir m))))
-        (struct-copy monde m
-                     (serpent serpent2)
-                     (perdu? (collision? serpent2))))))
+
+;;;;;;;;;;;;;;;;
+; Game loop
+;;;;;;;;;;;;;;;;
+
+
 
 (define (clavier m touche)
   (cond
@@ -101,7 +179,7 @@
      (struct-copy monde m (stop #t))]
     [#t m]))
 
-(big-bang m0+
+(big-bang m0
   (stop-when monde-stop)
   (close-on-stop #t)
   (on-key clavier)
